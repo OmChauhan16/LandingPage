744d9514ca3b0b40ef2ea4dc4543d79d378cf9f2
OmChauhan16 <omc5246@gmail.com>
2024-06-16 19:39:54 +0530

    assetsUpdated

--- LandingPage/src/Components/Body.jsx
+++ LandingPage/src/Components/Body.jsx
@@ -6,8 +6,8 @@ const Body = () => {
       <div className='flex items-center justify-center lg:flex-1 lg:order-2 lg:justify-end '>
         <img src="./src/assets/Blue-Shape.svg" alt="1st" className='-rotate-45 h-64 md:h-72 lg:h-[400px]' />
         <img src="./src/assets/Pink-Shape.svg" alt="2nd" className='absolute -rotate-[30deg] h-64 lg:h-[400px]' />
-        <img src="./src/assets/Purple-Shape.svg" alt="3rd" className='absolute -rotate-[15deg] h-64 md:h-72 lg:h-[400px]'  />
-        <img src="./src/assets/Hero-Model.png" alt="hero" className='absolute h-64 md:h-72 lg:h-[400px]' />
+        <img src="./Purple-Shape.svg" alt="3rd" className='absolute -rotate-[15deg] h-64 md:h-72 lg:h-[400px]'  />
+        <img src="./Hero-Model.png" alt="hero" className='absolute h-64 md:h-72 lg:h-[400px]' />
       </div>
       <div className='lg:flex-1 lg:order-1'>
        <h1 className='text-5xl font-bold font-playfair leading-tight'>Host your website in less than 5 minutes</h1>
--- LandingPage/vite.config.js
+++ LandingPage/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/LandingPage/', // This should match your repo name
+  build: {
+    outDir: 'dist'
+  }
 })
